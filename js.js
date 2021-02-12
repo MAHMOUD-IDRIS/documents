0055c1e57e80608983322bdfed98df5f43ab5d7a
Mahmoud Idris <mahmouddrs@gmail.com>
2021-02-12 07:31:39 +0000

    changing header

--- js.js
+++ js.js
@@ -1,58 +1,64 @@
-
+function CTHm(){
  // adding the certificate 
-    let mainDiv = document.querySelector('main');
+ let mainDiv = document.querySelector('main');
 
-    function addQulification (certificateName ,certificateSrc ){
-    const quali = document.createElement('div');
-    quali.className = 'qualificationContainer';
+ function addQulification (certificateName ,certificateSrc ){
+ const quali = document.createElement('div');
+ quali.className = 'qualificationContainer';
 
-    const img = document.createElement('img');
-    const title = document.createElement('div');
-    let destitle = document.createElement('h3');
-    const btn = document.createElement('div');
-    btn.innerText = "VIEW"
-    btn.id = 'btn'
-    
-    title.className = "des";
+ const img = document.createElement('img');
+ const title = document.createElement('div');
+ let destitle = document.createElement('h3');
+ const paragraphDescription = document.createElement('p');
+ paragraphDescription.innerText = "This is a description of the certificate"
+ const btn = document.createElement('div');
+ btn.innerText = "VIEW"
+ btn.id = 'btn'
+ 
+ title.className = "des";
 
-    destitle.textContent = certificateName;
-    title.appendChild(destitle);
-    title.appendChild(btn);
-    
-        
-    img.src = certificateSrc;
-    quali.appendChild(img);
-    quali.appendChild(title);
-    
-    mainDiv.appendChild(quali);
+ destitle.textContent = certificateName;
+ title.appendChild(destitle);
+ title.appendChild(paragraphDescription);
+ 
+     
+ img.src = certificateSrc;
+ quali.appendChild(img);
+ quali.appendChild(title);
+ 
+ mainDiv.appendChild(quali);
 
-    function visib (ele){
-        setTimeout(
-            ()=>{
-               ele.target.style.visibility = "visible";
-        console.log('I worked!') 
-            }, 5000
-        )
-    }
-    title.onclick = (ele) => {
-        console.log(ele);
-        ele.target.style.visibility = "hidden";
-        visib(ele);
-    }
-    
+ function visib (ele){
+     setTimeout(
+         ()=>{
+            ele.target.style.visibility = "visible";
+     console.log('I worked!') 
+         }, 5000
+     )
+ }
+ title.onclick = (ele) => {
+     console.log(ele);
+     title.style.visibility = "hidden";
+     visib(ele);
+ }
+ 
 };
-   
-console.log('endFile')
-   //fetching the json file
 
-    fetch ('json.json').then(res => res.json()).then (
-    data => {
-        console.log(data.certificates)
-        data.certificates.forEach(element => {
-            addQulification(element.name, element.src)
-        });
-    }) .catch ((e)=>console.log(e.message));
+console.log('endFile')
+//fetching the json file
+
+ fetch ('json.json').then(res => res.json()).then (
+ data => {
+     console.log(data.certificates)
+     data.certificates.forEach(element => {
+         addQulification(element.name, element.src)
+     });
+ }) .catch ((e)=>console.log(e.message));
 // end of adding certificate file;
 
 // event listners for view
 
+
+
+};
+CTHm();
